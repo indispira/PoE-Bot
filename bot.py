3e5e06faf88e4176cf84ab1f4c8239366ed7a810
Stéphane Boulet <stephaneboulet@Jasmines-MacBook-Air.local>
2018-01-04 11:32:41 +0100

    Add the duration and datetime detection

--- bot.py
+++ bot.py
@@ -17,21 +17,39 @@ def index():
   item_addr = string.replace(item_addr, "Orbs", "Orb")
   print item_name, item_addr
 
-  # Check if 'number' is present on the json
+  # Check if 'number' is present in the json
   if 'number' not in data['nlp']['entities']:
     number = 1
   else:
     number = data['nlp']['entities']['number'][0]['scalar']
   print number
 
+  # Check if 'duration' or 'datetime' are present in the json
+  time_mode = 0
+  datetime_array = {'hour':'1h', 'halfday':'12h', 'day':'1d', 'week':'7d'}
+  if 'datetime' not in data['nlp']['entities'] and 'duration' not in data['nlp']['entities']:
+    time = '1h'
+  elif 'datetime' in data['nlp']['entities'] and 'duration' not in data['nlp']['entities']:
+    time = datetime_array[data['nlp']['entities']['datetime'][0]['accuracy']]
+    time_mode = 1
+  elif 'duration' in data['nlp']['entities']:
+    if data['nlp']['entities']['duration'][0]['hours'] <= 1: time = '1h'
+    elif data['nlp']['entities']['duration'][0]['hours'] <= 6: time = '6h'
+    elif data['nlp']['entities']['duration'][0]['hours'] <= 12: time = '12h'
+    elif data['nlp']['entities']['duration'][0]['hours'] <= 24: time = '1d'
+    else: time = '7d'
+    time_mode = 2
+  print time
+
   # Call the page to be loaded
-  address = "http://poe-rates.com/index.php?league=Abyss&item="+item_addr+"&interval=1h"
+  address = "http://poe-rates.com/index.php?league=Abyss&item="+item_addr+"&interval="+time
   driver = webdriver.PhantomJS()
+  print address
 
   # Wait until the page is fully loaded
   waiting_time = 0
   value = -1
-  while value == -1 and waiting_time < 7:
+  while value == -1 and waiting_time < 8:
     waiting_time += 1
     driver.implicitly_wait(1)
     driver.get(address)
@@ -47,7 +65,7 @@ def index():
     print value
 
   # Configure the content of the message
-  if waiting_time > 6:
+  if waiting_time > 7 and value == -1:
     content = 'The website take too long to respond, try again later.'
   elif number != 1:
     # item_name = string.replace(item_name, "Orb", "Orbs")
--- test-json.py
+++ test-json.py
@@ -4,16 +4,21 @@ import requests
 from selenium import webdriver
 
 item = "Exalted%20Orb"
+datetime_array = {'hour':'1h', 'halfday':'12h', 'day':'1d', 'week':'7d'}
 
 def dump(obj):
    for attr in dir(obj):
        if hasattr( obj, attr ):
            print( "obj.%s = %s" % (attr, getattr(obj, attr)))
 
-# print(json.loads(request.get_data()))
-# r = requests.get("http://poe-rates.com/index.php?league=Abyss&item="+item+"&interval=1h")
+if 'datetime' not in data['nlp']['entities'] and 'duration' not in data['nlp']['entities']:
+	time = '1h'
+elif 'datetime' in data['nlp']['entities'] and 'duration' not in data['nlp']['entities']:
+	time = datetime_array[data['nlp']['entities']['datetime'][0]['accuracy']]
+# elif 'duration' in data['nlp']['entities']:
+print time
 
-address = "http://poe-rates.com/index.php?league=Abyss&item="+item+"&interval=1h"
+address = "http://poe-rates.com/index.php?league=Abyss&item="+item+"&interval="+time
 
 driver = webdriver.PhantomJS()
 driver.implicitly_wait(1)
@@ -27,3 +32,31 @@ for e in elems:
 		print e.text
 		value = e.text
 		break
+
+
+# "datetime": [
+#         {
+#           "formatted": "Thursday, 04 January 2018 at 07:00:00 AM (+0000)",
+#           "iso": "2018-01-04T07:00:00+00:00",
+#           "accuracy": "halfday",
+#           "chronology": "past",
+#           "state": "relative",
+#           "raw": "this morning",
+#           "confidence": 0.97
+#         }
+#       ]
+
+# "duration": [
+#         {
+#           "chrono": "06:00:00",
+#           "raw": "6 hours",
+#           "confidence": 0.59,
+#           "hours": 6,
+#           "months": 0.00821917808219178,
+#           "years": 0.000684844641724794,
+#           "days": 0.25,
+#           "minutes": 360,
+#           "seconds": 21600
+#         }
+#       ]
+
